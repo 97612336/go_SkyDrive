ef63060b9c857a8228f4070eba8c06687a2c00c0
wangkun <wangkun@autoforce.net>
2018-07-03 18:21:47 +0800

    最终版完成

--- config/config.go
+++ config/config.go
@@ -1,7 +1,7 @@
 package config
 
 //静态文件存放的本地地址
-var Static_Path = "/home/wangkun/static_dir"
+var Static_Path = "/home/wangkun/static_dir/"
 //静态文件读取的网络地址前缀
 var Static_href = "http://localhost:3456/"
 
--- handlers/urls.go
+++ handlers/urls.go
@@ -8,6 +8,7 @@ import (
 
 func MyUrls() {
 	http.HandleFunc("/", index.Index)
-	http.HandleFunc("/manager/advert/add_advert",adv.Add_adver)
-	http.HandleFunc("/add",adv.Add)
+	http.HandleFunc("/manager/advert/add_advert", adv.Add_adver)
+	http.HandleFunc("/add_file", index.Add_file)
+	http.HandleFunc("/upload_file",index.Upload_file)
 }
